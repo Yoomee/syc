8d2982366044fd5c14f4c324d27720baf55d4dae
Simon Wilkins <simonwilkins@sislocalhost.com>
2010-07-07 15:26:49 +0100

    Fix chain ordering

--- client/app/controllers/sections_controller.rb
+++ client/app/controllers/sections_controller.rb
@@ -1,12 +1,15 @@
 SectionsController.class_eval do
   
-  around_filter :set_news_section_items_per_page, :only => :show
+  before_filter :set_news_section_items_per_page, :only => :show
+  after_filter :reset_news_section_items_per_page, :only => :show
   
   private
   def set_news_section_items_per_page
-    per_page = APP_CONFIG[:latest_stories_items_per_page]
+    @per_page = APP_CONFIG[:latest_stories_items_per_page]
     APP_CONFIG[:latest_stories_items_per_page] = 4 if @section == Section::news
-    yield
+  end
+  
+  def reset_news_section_items_per_page
     APP_CONFIG[:latest_stories_items_per_page] = per_page
   end
   
