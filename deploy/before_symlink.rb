5a987aac52389a978618c3ba497b0529b7a781b1
Simon Wilkins <simonwilkins@sislocalhost.com>
2010-11-10 13:23:50 +0000

    Change to deploy hook

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,4 @@
 run "rm -rf #{release_path}/public/uploads"
-run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
 (2010..Time.now.year).each do |year|
   release_year_path = "#{release_path}/public/dragonfly/#{year}"
   shared_year_path = "#{shared_path}/dragonfly/#{year}"
@@ -7,4 +6,4 @@ run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
   run "mkdir -p #{shared_year_path}" unless File.exists?(shared_year_path)
   run "ln -nfs #{shared_year_path} #{release_year_path}"
 end
-run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
+run "ln -nfs #{shared_path}/uploads #{release_path}/uploads"
